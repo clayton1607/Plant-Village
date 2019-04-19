065bb7210bbb4472d7a8bc7952937965c814ab3d
Clayton <claytonpereira1998@gmail.com>
2019-04-20 01:52:18 +0530

    url image

--- server.js
+++ server.js
@@ -116,73 +116,27 @@ app.get('/queries',(req,res)=>{
   res.send("HELLO");
 })
 
-const multer = require("multer");
+app.post("/askqts",(req, res) => {
+    var questionp= req.body.yourq; //question that is asked
+    var type= req.body.type; //type of question
+    var name= req.body.name; //who asked the question
+          //id of the question will be on auto increment in database
+    var targetPath = req.body.image_path;
+    var q = [questionp] //for second query
+    var values1 = [questionp,name,type,targetPath];    
+    question.query('insert into q2table (questions,quser,type,image_path) values ($1,$2,$3,$4)',values1,(err,row)=>{
+      if(err) throw err;
+      question.query('select qno from q2table where questions= $1',q,(err,result)=>{
+        if(err) throw err;
+        var values2 = [result.rows[0].qno,"This Discussion is ready","Admin","default"];
+        question.query('insert into ans3table (qno,answer,usera,type) values ($1,$2,$3,$4)',values2,(err,rows)=>{
+          if(err) throw err;
+          res.send('question is added');
+        });
+      }); 
+    });
+  });
 
-const handleError = (err, res) => {
-    res.status(500)
-    res.contentType("text/plain")
-    res.end("Oops! Something went wrong!");
-};
-// const upload = multer({
-//   dest: "/uploads"
-//   // you might also want to set some limits: https://github.com/expressjs/multer#limits
-// });
-// app.post("/upload",upload.single("file" /* name attribute of <file> element in your form */),(req, res) => {
-//     var questionp= req.body.yourq; //question that is asked
-//           var type= req.body.type; //type of question
-//           var name= req.body.name; //who asked the question
-//           //id of the question will be on auto increment in database
-//       var q = [questionp] //for second query
-//     console.log(req.file)
-//     console.log(req.file.path);
-//     const tempPath = req.file.path;
-//     var image=Math.floor(100000 + Math.random() * 900000);
-//     var img=image.toString();
-//     var png ="image.png";
-//     var image_png=img.concat(png);
-//     const targetPath = path.join(__dirname, "public/uploads",image_png);
-//     console.log("IM here");
-//     console.log(tempPath);
-//     console.log("IM here");
-//     console.log(targetPath);
-//     var values1 = [questionp,name,type,targetPath];
-
-//     if (path.extname(req.file.originalname).toLowerCase() === ".png") {
-//       fs.rename(tempPath, targetPath, err => {
-//         if (err) return handleError(err, res);
-//            res.status(200)
-//           res.contentType("text/plain")
-//           // res.send("File uploaded!");
-          
-//           question.query('insert into q2table (questions,quser,type,image_path) values ($1,$2,$3,$4)',values1,(err,row)=>{
-//             if(err) throw err;
-       
-//             question.query('select qno from q2table where questions= $1',questionp,(err,result)=>{
-//               if(err) throw err;
-//               var values2 = [result.rows[0].qno,"This Discussion is ready","Admin","default"];
-//               question.query('insert into anstable (qno,answer,usera,type) values ($1,$2,$3,$4)',values2,(err,rows)=>{
-//                 if(err) throw err;
-//                 res.send('question is added');
-//               });
-//             });
-       
-       
-       
-       
-//           });
-//       });
-//     } else {
-//       fs.unlink(tempPath, err => {
-//         if (err) return handleError(err, res);
-
-//         res
-//           .status(403)
-//           .contentType("text/plain")
-//           .end("Only .png files are allowed!");
-//       });
-//     }
-//   }
-// );
 
 app.post('/reply2',(req,res)=>{
   var replystring= req.body.replyf;
@@ -207,10 +161,6 @@ app.post('/reply2',(req,res)=>{
  app.get('/listans',(req,res,err)=>{
 
           var askno=[req.query.quno];
-        
-
-        
-        
        
         question.query('select * from ans3table where qno= $1',askno,(err,result)=>{
            if(err) throw err;
@@ -528,7 +478,7 @@ app.post('/reply2',(req,res)=>{
 // });
 
 // app.listen(3000);
-app.listen(port);
+app.listen(process.env.PORT);
 
 
 
